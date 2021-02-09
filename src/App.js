bc4ae868f5eb58c724d4fee432b825ef01a57c60
chuang0830 <eilleen50830@gmail.com>
2021-02-09 10:30:51 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 //元件
-// import Menu from './components/Menu'
-// import MyFooter from './components/MyFooter'
 
 //頁面
-// import UserList from './pages/UserList'
-// import UserEdit from './pages/UserEdit'
-//import UserAdd from './pages/UserAdd'
-import MemberLogin from './pages/MemberLogin'
+import Login from './pages/Login'
 import ForgetPass from './pages/ForgetPass'
+import Register from './pages/Register'
+import MemberEdit from './pages/MemberEdit'
+import MemberOrderList from './pages/MemberOrderList'
+import MemberFav from './pages/MemberFav'
+import MemberCoupon from './pages/MemberCoupon'
 
 function App() {
   return (
     <Router>
       <>
-        <MemberLogin />
-
         <Switch>
           <Route exact path="/user-forgetpass">
             <ForgetPass />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/memberedit">
+            <MemberEdit />
+          </Route>
+          <Route path="/memberorderList">
+            <MemberOrderList />
+          </Route>
+          <Route path="/memberfav">
+            <MemberFav />
+          </Route>
+          <Route path="/membercoupon">
+            <MemberCoupon />
           </Route>
         </Switch>
       </>
