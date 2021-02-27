ee52a2274cdf44dfc01560632870ea851861a964
chuang0830 <eilleen50830@gmail.com>
2021-02-27 15:07:34 +0800

    camera icon became small

--- src/pages/MMemberEdit.js
+++ src/pages/MMemberEdit.js
@@ -168,8 +168,8 @@ function MMemberEdit(props) {
               <button onClick={onButtonClick}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
+                  width="35"
+                  height="35"
                   viewBox="0 0 50 50"
                 >
                   <g
